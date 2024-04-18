70508d24984587cd982ab82a5aee700dec9681ed
Arno Temmerman <arno.temmer@gmail.com>
2024-04-18 10:07:32 +0200

    added TensorBoard logging to base_regressor

--- models/base_regressor.py
+++ models/base_regressor.py
@@ -4,6 +4,7 @@ import time
 
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 
 class BaseRegressor(nn.Module):
     def __init__(self, input_size, hidden_size, component):
@@ -19,6 +20,8 @@ class BaseRegressor(nn.Module):
 
         self.component = component
         self.timestamp = time.strftime("%Y%m%d/%H%M%S", time.localtime())
+
+        self.writer = SummaryWriter(Path(f'../results/{self.component}/{self.timestamp}'))
 
 
     def forward(self, x):
@@ -41,6 +44,14 @@ class BaseRegressor(nn.Module):
 
             # Adjust the NN's weights and biases accordingly
             optimizer.step()
+
+            # Log the loss to TensorBoard
+            self.writer.add_scalar(f'Training/MSE', loss.item(), epoch)
+
+            # Log the weights and biases to TensorBoard
+            for name, param in self.named_parameters():
+                self.writer.add_histogram(name, param.clone().cpu(), epoch)
+                self.writer.add_histogram(name + '/grad_norm', param.grad.data.norm(), epoch)
 
             if epoch % 10 == 0:
                 print(f'[epoch:{epoch}]: MSE = {loss}')
