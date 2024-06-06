4de800214006847d9682831ac8b9e62ac9ec9168
Arno Temmerman <arno.temmer@gmail.com>
2024-06-06 21:19:19 +0200

    updated hyperparam_tuning multi

--- 3_multi-task_method/hyperparameter-tuning.py
+++ 3_multi-task_method/hyperparameter-tuning.py
@@ -34,7 +34,7 @@ gait_cycles = dp.read_gait_cycles(DATA_DIR, SUBJECTS, SCENES, TRIALS, drop_emgs=
 ############################
 # TRIMMING THE GAIT CYCLES #
 ############################
-df_l, df_r = dp.filter_seperately(gait_cycles)
+df = dp.filter_together(gait_cycles)
 
 del gait_cycles
 
@@ -42,33 +42,36 @@ del gait_cycles
 ##############################################
 # LEFT VS. RIGHT FOOT -> MAIN VS. OTHER FOOT #
 ##############################################
-df_l, df_r = dp.homogenize(df_l, df_r)
-df_homogenous = pd.concat([df_l, df_r])
+df_mirror = dp.reformat(df, 'r', 'l')
 
-del df_l, df_r
+df_l, df_r = dp.homogenize(df)
+df_augmented = pd.concat([df_l, df_r])
+
+del df, df_l, df_r
 
 
 ############################
 # FEATURE/LABEL EXTRACTION #
 ############################
 # Features
-X = dp.extract_features(df_homogenous)
+X = dp.extract_features(df_augmented)
 
 # Labels
-LABELS = ['Fx', 'Fy', 'Fz', 'Tz']
-Y = df_homogenous[LABELS]
+LABELS = ['Fx', 'Fy', 'Fz', 'Tz',
+          'Fx_o', 'Fy_o', 'Fz_o', 'Tz_o',]
+Y = df_augmented[LABELS]
 
 # Subjects
-subjects = df_homogenous['subject']
+subjects = df_augmented['subject']
 
-del df_homogenous
+del df_augmented
 
 
 ######################
 # CONVERT TO TENSORS #
 ######################
 X_tensor = torch.tensor(X.values, dtype=torch.float32)
-Y_tensor = torch.tensor(Y.to_numpy().reshape((-1, 4)), dtype=torch.float32)
+Y_tensor = torch.tensor(Y.to_numpy().reshape((-1, 8)), dtype=torch.float32)
 
 del X, Y
 
@@ -83,39 +86,36 @@ kf = LeaveOneGroupOut()
 # #########################
 # # HYPERPARAMETER TUNING #
 # #########################
-best_models = {}
+# 1. Define an objective function to be maximized
+def objective(trial):
+    # 2. Suggest values for the hyperparameters using a trial object
+    ## a. Number of layers
+    n_layers = trial.suggest_int('hidden_layers', 1, 2)
 
-for i, label in enumerate(LABELS):
-    # 1. Define an objective function to be maximized
-    def objective(trial):
-        # 2. Suggest values for the hyperparameters using a trial object
-        ## a. Number of layers
-        n_layers = trial.suggest_int('hidden_layers', 1, 2)
+    ## b. Number of neurons per layer
+    hidden_sizes = []
+    for i in range(n_layers):
+        size = trial.suggest_int(f'hidden_size_{i}', 32 * 8, 64 * 8)
+        hidden_sizes.append(size)
 
-        ## b. Number of neurons per layer
-        hidden_sizes = []
-        for i in range(n_layers):
-            size = trial.suggest_int(f'hidden_size_{i}', 32 * 4, 64 * 4)
-            hidden_sizes.append(size)
+    # 3. Instantiate a model with suggested hyperparameters
+    model = MLP(hidden_sizes)
+    trial.set_user_attr('hidden_sizes', hidden_sizes)
 
-        # 3. Instantiate a model with suggested hyperparameters
-        model = MLP(hidden_sizes)
-        trial.set_user_attr('hidden_sizes', hidden_sizes)
-
-        # 4. Cross-validate the suggested model
-        mses, corrs = me.cross_validate(model, X_tensor, Y_tensor.reshape(-1, 4), subjects, kf)
-        print('Out-of-sample errors:', mses)
-        print('Correlations:', corrs)
-        return np.mean(mses)
+    # 4. Cross-validate the suggested model
+    mses, corrs = me.cross_validate(model, X_tensor, Y_tensor.reshape(-1, 8), subjects, kf)
+    print('Out-of-sample errors:', mses)
+    print('Correlations:', corrs)
+    return np.mean(mses)
 
 
-    # 5. Create a study object and optimize the objective function.
-    study = optuna.create_study(study_name=label, direction='minimize')
-    study.set_user_attr('best_score', float('inf'))
-    study.optimize(objective, n_trials=50)
+# 5. Create a study object and optimize the objective function.
+study = optuna.create_study(study_name='Y', direction='minimize')
+study.set_user_attr('best_score', float('inf'))
+study.optimize(objective, n_trials=50)
 
-    # Remember the best model
-    best_hyperparams = study.best_trial.user_attrs['hidden_sizes']
+# Remember the best model
+best_hyperparams = study.best_trial.user_attrs['hidden_sizes']
 
 
 ######################
@@ -150,5 +150,5 @@ del X_tensor, scaler, X_scaled, pca, X_pc
 
 # TRAIN AND SAVE THE  MODELS #
 model = MLP(best_hyperparams)
-model.train_(X_pc_tensor, Y_tensor[:, i].reshape(-1, 1))
+model.train_(X_pc_tensor, Y_tensor.reshape(-1, 8))
 model.save(DIR, 'Y')
